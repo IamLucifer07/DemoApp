042a0fd40a048b576f37a516c8c08d297ea429a5
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-05-13 20:58:32 +0545

    nav component

--- resources/js/components/ui/navigation-menu/NavigationMenuItem.vue
+++ resources/js/components/ui/navigation-menu/NavigationMenuItem.vue
@@ -10,5 +10,11 @@ const delegatedProps = reactiveOmit(props, 'class')
 </script>
 
 <template>
-  
+  <NavigationMenuItem
+    data-slot="navigation-menu-item"
+    v-bind="delegatedProps"
+    :class="cn('relative', props.class)"
+  >
+    <slot />
+  </NavigationMenuItem>
 </template>
